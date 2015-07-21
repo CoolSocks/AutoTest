a57c25226c713d56b502fc68da4dfeda4c75c8bd
The Great Dane <danesboucher@gmail.com>
2015-07-21 14:37:05 -0400

    Changing some test configs

--- fixtures/test/spec/publish/testconfig.js
+++ fixtures/test/spec/publish/testconfig.js
@@ -1,5 +1,5 @@
-exports.browser1='chrome';
-exports.sauce1=false;
+eports.browser1='chrome';
+exports.sauce1=true;
 exports.version1='30';
 exports.platform1='Windows 7';
 exports.portnum1=4444;
@@ -8,5 +8,5 @@ exports.domain_local='http://localhost:4502';
 exports.domain_author='http://cmsautdev.acs.org';
 exports.domain_publish='http://cmspubdev.acs.org';
 exports.username='admin';
-exports.password='admin';
-//exports.password='cq<>acs.org';
+//exports.password='admin';
+exports.password='cq<>acs.org';
