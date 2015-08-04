d68e0c706eca0719e3ae5ae477e8aa382a0920e1
The Great Dane <danesboucher@gmail.com>
2015-08-04 14:12:37 -0400

    Testconfig change

--- fixtures/test/spec/publish/testconfig.js
+++ fixtures/test/spec/publish/testconfig.js
@@ -1,9 +1,16 @@
+
 exports.browser1='chrome';
 exports.sauce1=true;
 exports.version1='30';
 exports.platform1='Windows 7';
 exports.portnum1=4444;
-exports.domain='http://cmspubdev.acs.org';
+exports.bamboodomain='${env.BAMBOO.PUBLISHHOST}';
+if((!exports.bamboodomain || !exports.bamboodomain.length)){
+	exports.domain='http://cmspubdev.acs.org';
+}else{
+	exports.domain=exports.bamboodomain;
+}
+
 exports.domain_local='http://localhost:4502';
 exports.domain_author='http://cmsautdev.acs.org';
 exports.domain_publish='http://cmspubdev.acs.org';
