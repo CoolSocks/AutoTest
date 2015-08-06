7609b27e93cf9e3869e4f3f27b7cddeb007c6b8b
The Great Dane <danesboucher@gmail.com>
2015-08-06 17:33:33 -0400

    foo

--- fixtures/test/spec/publish/testconfig.js
+++ fixtures/test/spec/publish/testconfig.js
@@ -3,7 +3,6 @@ exports.sauce1=true;
 exports.version1='30';
 exports.platform1='Windows 7';
 exports.portnum1=4444;
-exports.domain='${bamboo.cqAuthorHost}';
 exports.domain_local='http://localhost:4502';
 exports.domain_author='http://cmsautdev.acs.org';
 exports.domain_publish='http://cmspubdev.acs.org';
