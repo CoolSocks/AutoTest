bda7f5334a2999b05f34d838838b95a65be12a3b
The Great Dane <danesboucher@gmail.com>
2015-08-06 12:21:42 -0400

    Stuff

--- fixtures/test/spec/publish/testconfig.js
+++ fixtures/test/spec/publish/testconfig.js
@@ -4,7 +4,7 @@ exports.sauce1=true;
 exports.version1='30';
 exports.platform1='Windows 7';
 exports.portnum1=4444;
-exports.bamboodomain='${env.BAMBOO.PUBLISHHOST}';
+exports.bamboodomain='${bamboo.cqAuthorHost}';
 if((!exports.bamboodomain || !exports.bamboodomain.length)){
 	exports.domain='http://cmspubdev.acs.org';
 }else{
